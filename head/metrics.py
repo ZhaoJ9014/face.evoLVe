6b6ffcdafcd75589295cb5a3f1da0b877b562184
Zhao Jian <zhaojian90@u.nus.edu>
2019-01-05 15:29:07 +0800

    Update metrics.py

--- head/metrics.py
+++ head/metrics.py
@@ -153,3 +153,38 @@ class SphereFace(nn.Module):
                + 'in_features=' + str(self.in_features) \
                + ', out_features=' + str(self.out_features) \
                + ', m=' + str(self.m) + ')'
+
+    
+class Am_softmax(Module):
+    r"""Implement of SphereFace:
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        m: margin
+        s: scale of outputs
+    """   
+    
+    def __init__(self, in_features, out_features, m=0.35, s=30.):
+        super(Am_softmax, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.kernel = Parameter(torch.Tensor(self.in_features, self.out_features))
+        # initial kernel
+        self.kernel.data.uniform_(-1, 1).renorm_(2,1,1e-5).mul_(1e5)
+        self.m = m
+        self.s = s
+        
+    def forward(self,embbedings,label):
+        kernel_norm = l2_norm(self.kernel,axis=0)
+        cos_theta = torch.mm(embbedings,kernel_norm)
+        cos_theta = cos_theta.clamp(-1,1) # for numerical stability
+        phi = cos_theta - self.m
+        label = label.view(-1,1) # size=(B,1)
+        index = cos_theta.data * 0.0 # size=(B,Classnum)
+        index.scatter_(1,label.data.view(-1,1),1)
+        index = index.byte()
+        output = cos_theta * 1.0
+        output[index] = phi[index] # only change the correct predicted output
+        output *= self.s # scale up in order to make softmax work, first introduced in normface
+        
+        return output
