aa65dc1f569e5edd67e98dbbc7fbc20a7f556dbe
Zhao Jian <zhaojian90@u.nus.edu>
2019-01-05 16:12:03 +0800

    Update utils.py

--- util/utils.py
+++ util/utils.py
@@ -70,6 +70,20 @@ def separate_irse_bn_paras(modules):
     return paras_only_bn, paras_wo_bn
 
 
+def separate_resnet_bn_paras(modules):
+    all_parameters = modules.parameters()
+    paras_only_bn = []
+
+    for pname, p in modules.named_parameters():
+        if pname.find('bn') >= 0:
+            paras_only_bn.append(p)
+            
+    paras_only_bn_id = list(map(id, paras_only_bn))
+    paras_wo_bn = list(filter(lambda p: id(p) not in paras_only_bn_id, all_parameters))
+    
+    return paras_only_bn, paras_wo_bn
+
+
 def de_preprocess(tensor):
     return tensor*0.5 + 0.5
 
