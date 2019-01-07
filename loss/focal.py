5bff553cdb4ef81fde4842f6d3099614f8be2960
Zhao Jian <zhaojian90@u.nus.edu>
2019-01-07 20:44:39 +0800

    Add files via upload

---
+++ loss/focal.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+
+# Support: ['FocalLoss']
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma = 2, eps = 1e-7):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.eps = eps
+        self.ce = nn.CrossEntropyLoss()
+
+    def forward(self, input, target):
+        logp = self.ce(input, target)
+        p = torch.exp(-logp)
+        loss = (1 - p) ** self.gamma * logp
+        return loss.mean()
