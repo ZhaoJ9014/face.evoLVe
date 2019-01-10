d78f8fee23076591510fc296671312a339c98ccc
Zhao Jian <zhaojian90@u.nus.edu>
2019-01-10 10:43:47 +0800

    Update metrics.py

--- head/metrics.py
+++ head/metrics.py
@@ -7,7 +7,44 @@ from torch.nn import Parameter
 import math
 
 
-# Support: ['ArcFace', 'CosFace', 'SphereFace', 'Am_softmax']
+# Support: ['Softmax', 'ArcFace', 'CosFace', 'SphereFace', 'Am_softmax']
+
+
+class Softmax(nn.Module):
+    r"""Implement of Softmax (normal classification head):
+        Args:
+            in_features: size of each input sample
+            out_features: size of each output sample
+        """
+    def __init__(self, in_features, out_features):
+        super(Softmax, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.fc = nn.Linear(self.in_features, self.out_features)
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        out = self.fc(x)
+
+        return out
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
 
 
 class ArcFace(nn.Module):
