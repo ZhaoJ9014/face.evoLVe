3643b6eed5a0990c8dead862041a3f9c5e2dd23e
Zhao Jian <zhaojian90@u.nus.edu>
2019-01-12 18:36:54 +0800

    Update train.py

--- train.py
+++ train.py
@@ -175,9 +175,6 @@ if __name__ == '__main__':
         if epoch == STAGES[2]:
             schedule_lr(OPTIMIZER)
 
-        running_loss = 0.0
-        running_corrects = 0
-
         losses = AverageMeter()
         top1 = AverageMeter()
         top5 = AverageMeter()
