7cb006d41e84dd7ce65276771a872022817bac35
Zhao Jian <zhaojian90@u.nus.edu>
2019-01-22 15:29:51 +0800

    Add files via upload

---
+++ util/extract_feature_v2.py
@@ -0,0 +1,64 @@
+# Helper function for extracting features from pre-trained models
+import torch
+import cv2
+import numpy as np
+import os
+
+
+def l2_norm(input, axis = 1):
+    norm = torch.norm(input, 2, axis, True)
+    output = torch.div(input, norm)
+
+    return output
+
+
+def extract_feature(img_root, backbone, model_root, device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"), tta = True):
+    # pre-requisites
+    assert(os.path.exists(img_root))
+    print('Testing Data Root:', img_root)
+    assert (os.path.exists(model_root))
+    print('Backbone Model Root:', model_root)
+
+    # load image
+    img = cv2.imread('./test/1/111.jpg')
+
+    # resize image to [128, 128]
+    resized = cv2.resize(img, (128, 128))
+
+    # center crop image
+    a=int((128-112)/2) # x start
+    b=int((128-112)/2+112) # x end
+    c=int((128-112)/2) # y start
+    d=int((128-112)/2+112) # y end
+    ccropped = resized[a:b,c:d] #crop the image
+    ccropped = ccropped[...,::-1]
+
+    # flip image horizontally
+    flipped = cv2.flip(ccropped, 1)
+
+    # load numpy to tensor
+    ccropped = np.reshape(ccropped, [1, 3, 112, 112])
+    ccropped = np.array(ccropped, dtype=np.float32)
+    ccropped = (ccropped - 128) / 128.0
+    ccropped = torch.from_numpy(ccropped)
+
+    flipped = np.reshape(flipped, [1, 3, 112, 112])
+    flipped = np.array(flipped, dtype=np.float32)
+    flipped = (ccropped - 128) / 128.0
+    flipped = torch.from_numpy(flipped)
+
+
+    # load backbone from a checkpoint
+    print("Loading Backbone Checkpoint '{}'".format(model_root))
+    backbone.load_state_dict(torch.load(model_root))
+
+    # extract features
+    backbone.eval() # set to evaluation mode
+    with torch.no_grad():
+        if tta:
+            emb_batch = backbone(ccropped.to(device)).cpu() + backbone(flipped.to(device)).cpu()
+            features = l2_norm(emb_batch)
+        else:
+            features = backbone(ccropped.to(device)).cpu()
+
+    return features
