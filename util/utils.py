b687c0bee156d9ca3d2f3bac7a6afc3711f1a184
Zhao Jian <zhaojian90@u.nus.edu>
2019-02-21 08:37:40 +0800

    Update utils.py

--- util/utils.py
+++ util/utils.py
@@ -204,7 +204,7 @@ def perform_val(multi_gpu, device, embedding_size, batch_size, backbone, carray,
                 embeddings[idx:] = l2_norm(emb_batch)
             else:
                 ccropped = ccrop_batch(batch)
-                embeddings[idx:] = l2_normbackbone(ccropped.to(device))).cpu()
+                embeddings[idx:] = l2_norm(backbone(ccropped.to(device))).cpu()
 
     tpr, fpr, accuracy, best_thresholds = evaluate(embeddings, issame, nrof_folds)
     buf = gen_plot(fpr, tpr)
