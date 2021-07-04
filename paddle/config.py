ac982b17a49762cd92ac99f0812b223ab2197d1c
Reatris <reatris@qq.com>
2021-07-04 21:31:34 +0800

    add README.md

--- paddle/config.py
+++ paddle/config.py
@@ -3,7 +3,7 @@ configurations = {
     1: dict(
         SEED = 1337, # random seed for reproduce results
 
-        DATA_ROOT = 'defeault',
+        DATA_ROOT = 'data/lfw',
         #the parent root where your train/val/test data are stored
         # If you want to test quickly, you can use the dataset ['defeault']
         # If you need to customize the dataset, please define it according to the following format.
@@ -25,11 +25,11 @@ configurations = {
         BACKBONE_RESUME_ROOT = './', # the root to resume training from a saved checkpoint
         HEAD_RESUME_ROOT = './', # the root to resume training from a saved checkpoint
 
-        BACKBONE_NAME = 'ppResNet_50', # support: ['ppResNet_50','ResNet_50','ResNet_50', 'ppResNet_50',ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
-        HEAD_NAME = 'SphereFace', # support:  ['ArcFace', 'CosFace', 'SphereFace', 'Am_softmax',Softmax]
+        BACKBONE_NAME = 'ppResNet_50', # support: ['ppResNet_50','ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
+        HEAD_NAME = 'ArcFace', # support:  ['ArcFace', 'CosFace', 'SphereFace', 'Am_softmax','Softmax']
         LOSS_NAME = 'Focal', # support: ['Focal', 'Softmax']
 
-        INPUT_SIZE = [112, 112], # support: [112, 112] and [224, 224]
+        INPUT_SIZE = [112, 112], # support: [112, 112]
         RGB_MEAN = [0.5, 0.5, 0.5], # for normalize inputs to [-1, 1]
         RGB_STD = [0.5, 0.5, 0.5],
         EMBEDDING_SIZE = 512, # feature dimension
